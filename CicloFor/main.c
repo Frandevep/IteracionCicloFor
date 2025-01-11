e507848bd52842f0c0a31df77eb38ff5d05f3ffc
Frandevep <frandevep@gmail.com>
2025-01-11 00:01:15 -0300

    Add files via upload

---
+++ CicloFor/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int i=0;
+    // Usamos un bucle for para contar del 1 al 5
+    for (i=1; i<=5; i++) //verificamos si i <= 5, y aumentamos i en 1
+    {
+        printf("Contador: %d\n", i); // Mostramos el valor actual de i
+    }
+
+    return 0;
+}
